121fa452df71205653d6de0d375b5a931fa5b3a3
lyx-poly <201992Y@mymail.nyp.edu.sg>
2022-02-24 13:26:56 +0800

    Chest collision fixed (Only need to fix dialogue bug now)

--- App/Source/Scene3D/Entities/SolidObjectManager.cpp
+++ App/Source/Scene3D/Entities/SolidObjectManager.cpp
@@ -324,17 +324,28 @@ bool CSolidObjectManager::CheckForCollision(void)
 
 				if ((((*it)->GetType() == CSolidObject::TYPE::PLAYER)) && ((*it_other)->GetType() == CSolidObject::TYPE::CHEST) /* && CKeyboardController::GetInstance()->IsKeyPressed(GLFW_KEY_F)*/)
 				{
-					cPlayer3D->chest_near = true;
 					(*it)->RollbackPosition();
+					break;
+				}
+
+				if ((((*it)->GetType() == CSolidObject::TYPE::PLAYER)) && ((*it_other)->GetType() == CSolidObject::TYPE::CHEST_ZONE) /* && CKeyboardController::GetInstance()->IsKeyPressed(GLFW_KEY_F)*/)
+				{
+					cPlayer3D->chest_near = true;
+					//(*it)->RollbackPosition();
 					if (CKeyboardController::GetInstance()->IsKeyPressed(GLFW_KEY_F))
 					{
 						//cPlayer3D->chest_near = true;
 						//(*it_other)->RollbackPosition();
-						//(*it_other)->SetStatus(false);
+						(*it_other)->SetStatus(false);
 						cout << "** chest bonus received ***" << endl;
 						cPlayer3D->chest_near = false;
 						break;
 					}
+				}
+
+				if (((((*it)->GetType() == CSolidObject::TYPE::PLAYER)) && ((*it_other)->GetType() == CSolidObject::TYPE::CHEST_ZONE)) == false /* && CKeyboardController::GetInstance()->IsKeyPressed(GLFW_KEY_F)*/)
+				{
+					//cPlayer3D->chest_near = false;
 				}
 
 				if ((((*it)->GetType() == CSolidObject::TYPE::PLAYER)) && ((*it_other)->GetType() == CSolidObject::TYPE::DEFPOWER) /*&& CKeyboardController::GetInstance()->IsKeyPressed(GLFW_KEY_F)*/)
