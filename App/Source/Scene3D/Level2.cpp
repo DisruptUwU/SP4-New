0750a167783928bca415cacb3410f1feb1ea7729
zshaqeel <69779056+zshaqeel@users.noreply.github.com>
2022-02-25 16:04:46 +0800

    completely stop all sound in level 2

--- App/Source/Scene3D/Level2.cpp
+++ App/Source/Scene3D/Level2.cpp
@@ -649,7 +649,7 @@ bool CLevel2::Update(const double dElapsedTime)
 
 	if (cPlayer3D->playerlostallhealth == true)
 	{
-		cPlayer3D->playerhealthbelow30 = false;
+		//cPlayer3D->playerhealthbelow30 = false;
 		CCameraEffectsManager::GetInstance()->Get("Youlose")->SetStatus(true);
 		if (checkplayerdie == 0)
 		{
@@ -658,10 +658,11 @@ bool CLevel2::Update(const double dElapsedTime)
 		}
 		else
 		{
-
+			cSoundController->StopSound();
 		}
 		cPlayer3D->playerhealthbelow30 = false;
 		cGUI_Scene3D->gameOver = true;
+		cPlayer3D->playerhealthbelow30 = false;
 		loseGame = true;
 	}
 
